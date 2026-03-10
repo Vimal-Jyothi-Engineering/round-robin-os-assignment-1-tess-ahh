044287c4d0b344cba2cee781caa8facf7e4e8483
tess-ahh <tessamariya295@gmail.com>
2026-03-10 21:44:32 +0530

    Refactor process handling and input/output structure

--- rr.c
+++ rr.c
@@ -1,69 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-#define MAX 100
+struct process {
+    char pid[10];
+    int at, bt, rt;
+    int ct, wt, tat;
+};
 
 int main() {
-    int n, q;
-    int pid[MAX], at[MAX], bt[MAX], rt[MAX];
-    int ct[MAX], tat[MAX], wt[MAX];
-    int completed = 0, time = 0;
-    int done[MAX];
-
+    int n;
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d %d %d", &pid[i], &at[i], &bt[i]);
-        rt[i] = bt[i];
-        done[i] = 0;
+    struct process p[20];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
+        p[i].rt = p[i].bt;
     }
 
-    scanf("%d", &q);
+  ..
+    ..
+    ..
+    .
+    ..
+    
+   
+                 printf("Waiting Time:\n");
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].wt);
 
-    while (completed < n) {
-        int progress = 0;
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++)
+        printf("%s %d\n", p[i].pid, p[i].tat);
 
-        for (int i = 0; i < n; i++) {
-            if (done[i] == 0 && at[i] <= time && rt[i] > 0) {
-                progress = 1;
-
-                if (rt[i] > q) {
-                    time += q;
-                    rt[i] -= q;
-                } else {
-                    time += rt[i];
-                    rt[i] = 0;
-                    ct[i] = time;
-                    done[i] = 1;
-                    completed++;
-                }
-            }
-        }
-
-        if (!progress) {
-            time++;
-        }
-    }
-
-    float total_wt = 0, total_tat = 0;
-    for (int i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i];
-        wt[i]  = tat[i] - bt[i];
-        total_tat += tat[i];
-        total_wt  += wt[i];
-    }
-
-    printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d %d\n", pid[i], wt[i]);
-    }
-
-    printf("\nTurnaround Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d %d\n", pid[i], tat[i]);
-    }
-
-    printf("\nAverage Waiting Time: %.2f\n", total_wt / n);
-    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+    printf("Average Waiting Time: %.2f\n", avg_wt/n);
+    printf("Average Turnaround Time: %.2f\n", avg_tat/n);
 
     return 0;
 }
